dd8ddb2ca38a2e70f7924e599fa96b058cc6e29f
Rafael Perdigão <2221442@my.ipleiria.pt>
2024-05-29 10:42:27 +0100

    h

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -6,23 +6,16 @@
     <div class="flex justify-center">
         <div class="my-4 p-6 bg-white dark:bg-gray-900 overflow-hidden
                     shadow-sm sm:rounded-lg text-gray-900 dark:text-gray-50">
-            <x-movies.filter-card
-                :filterAction="route('movies.index')"
-                :resetUrl="route('movies.index')"
-                :genres="$genres"
-                :genre="old('genre', $filterByGenre)"
-                :name="old('name', $filterByName)"
-                class="mb-6"
-                />
-            <div class="flex items-center gap-4 mb-4">
-                <x-button
-                    href="{{ route('movies.create') }}"
-                    text="Create a new movie"
-                    type="success"/>
-            </div>
+            @can('create', App\Models\Movie::class)
+                <div class="flex items-center gap-4 mb-4">
+                    <x-button
+                        href="{{ route('movies.create') }}"
+                        text="Create a new movie"
+                        type="success"/>
+                </div>
+            @endcan
             <div class="font-base text-sm text-gray-700 dark:text-gray-300">
                 <x-movies.table :movies="$movies"
-                    :showGenre="true"
                     :showView="true"
                     :showEdit="true"
                     :showDelete="true"
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,4 @@
 <?php
-
-// use Illuminate\Support\Facades\Route;
-
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\AdministrativeController;
 use App\Http\Controllers\MovieController;
@@ -17,7 +9,6 @@ use App\Http\Controllers\CartController;
 use App\Models\Screening;
 use App\Models\Customer;
 use App\Models\Movie;
-use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /* ----- PUBLIC ROUTES ----- */
@@ -32,25 +23,16 @@ Route::get('movies/{movie}/curriculum', [MovieController::class, 'showCurriculum
     ->name('movies.curriculum')
     ->can('viewCurriculum', Movie::class);
 
-
 /* ----- Non-Verified users ----- */
 Route::middleware('auth')->group(function () {
     Route::get('/password', [ProfileController::class, 'editPassword'])->name('profile.edit.password');
 });
 
 /* ----- Verified users ----- */
-Route::middleware('auth', 'verified')->group(function () {
-
-
-
-    /* ----- Non-Verified users ----- */
-    Route::middleware('auth')->group(function () {
-        Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-        Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    });
-
-
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     Route::view('/dashboard', 'dashboard')->name('dashboard');
 
@@ -60,7 +42,7 @@ Route::middleware('auth', 'verified')->group(function () {
 
     //Movie resource routes are protected by MoviePolicy on the controller
     // The route 'show' is public (for anonymous user)
-    Route::resource('movies', MovieController::class)->except(['show']);
+    Route::resource('movies/showcase', MovieController::class)->except(['show']);
 
     //Department resource routes are protected by DepartmentPolicy on the controller
     Route::resource('departments', GenreController::class);
@@ -77,19 +59,10 @@ Route::middleware('auth', 'verified')->group(function () {
         ->name('customers.my')
         ->can('viewMy', Customer::class);
 
-    Route::delete('customers/{Customer}/photo', [CustomerController::class, 'destroyPhoto'])
-        ->name('customers.photo.destroy')
-        ->can('update', 'Customer');
-
-    //Customer resource routes are protected by CustomerPolicy on the controller
-    Route::resource('customers', CustomerController::class);
-
-    Route::get('customers/my', [CustomerController::class, 'myCustomers'])
-        ->name('customers.my')
-        ->can('viewMy', Customer::class);
     Route::delete('customers/{customer}/photo', [CustomerController::class, 'destroyPhoto'])
         ->name('customers.photo.destroy')
         ->can('update', 'customer');
+
     //Customer resource routes are protected by CustomerPolicy on the controller
     Route::resource('customers', CustomerController::class);
 
@@ -108,23 +81,19 @@ Route::middleware('auth', 'verified')->group(function () {
 
 /* ----- OTHER PUBLIC ROUTES ----- */
 
-// Use Cart routes should be accessible to the public */
+// Use Cart routes should be accessible to the public
 Route::middleware('can:use-cart')->group(function () {
     // Add a Screening to the cart:
-    Route::post('cart/{Screening}', [CartController::class, 'addToCart'])
+    Route::post('cart/{screening}', [CartController::class, 'addToCart'])
         ->name('cart.add');
     // Remove a Screening from the cart:
-    Route::delete('cart/{Screening}', [CartController::class, 'removeFromCart'])
+    Route::delete('cart/{screening}', [CartController::class, 'removeFromCart'])
         ->name('cart.remove');
     // Show the cart:
     Route::get('cart', [CartController::class, 'show'])->name('cart.show');
     // Clear the cart:
     Route::delete('cart', [CartController::class, 'destroy'])->name('cart.destroy');
 });
-
-
-//Movie show is public.
-Route::resource('movies', MovieController::class)->only(['show']);
 
 //Screenings index and show are public
 Route::resource('screenings', ScreeningController::class)->only(['index', 'show']);
