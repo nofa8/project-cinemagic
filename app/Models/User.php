4c5eefd4d1d3c33d834366941376300a2de52e04
Afonso Fernandes <dom.afonsoduarte@gmail.com>
2024-06-05 08:30:45 +0100

    gg

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,13 +8,12 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Database\Eloquent\Relations\HasOne;
-
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
-
+    use SoftDeletes;
     /**
      * The attributes that are mass assignable.
      *
