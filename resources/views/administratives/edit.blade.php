5798d1012d25fe5bea06e2e751c9a9165173dac1
Duarte Pedrosa <2221468@my.ipleiria.pt>
2024-06-15 15:24:12 +0100

    profileeeeeee

--- resources/views/administratives/edit.blade.php
+++ resources/views/administratives/edit.blade.php
@@ -7,30 +7,6 @@
     <div class="p-4 sm:p-8 bg-white dark:bg-gray-900 shadow sm:rounded-lg">
         <div class="max-full">
             <section>
-                <div class="flex flex-wrap justify-end items-center gap-4 mb-4">
-                    @can('create', App\Models\User::class)
-                    <x-button
-                        href="{{ route('administratives.create') }}"
-                        text="New"
-                        type="success"/>
-                    @endcan
-                    @can('view', $administrative)
-                    <x-button
-                        href="{{ route('administratives.show', ['administrative' => $administrative]) }}"
-                        text="View"
-                        type="info"/>
-                    @endcan
-                    @can('delete', $administrative)
-                    <form method="POST" action="{{ route('administratives.destroy', ['administrative' => $administrative]) }}">
-                        @csrf
-                        @method('DELETE')
-                        <x-button
-                            element="submit"
-                            text="Delete"
-                            type="danger"/>
-                    </form>
-                    @endcan
-                </div>
                 <header>
                     <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
                         Edit administrative "{{ $administrative->name }}"
