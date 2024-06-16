eb0f881c22862c3249cda1ba6eef4337f1352af7
Afonso Fernandes <dom.afonsoduarte@gmail.com>
2024-06-16 14:57:39 +0100

    a

--- resources/views/screenings/index.blade.php
+++ resources/views/screenings/index.blade.php
@@ -3,12 +3,6 @@
 @section('header-title', 'List of Screenings')
 
 @section('main')
-    
-    @can('create', App\Models\Movie::class)
-        <div class="flex items-center gap-4 mb-4">
-            <x-button href="{{ route('screenings.create') }}" text="Create a new Screening" type="success" />
-        </div>
-    @endcan
 
     <div class="grid grid-cols-1 gap-5">
         @each('screenings.shared.cards', $screenings, 'screening')
