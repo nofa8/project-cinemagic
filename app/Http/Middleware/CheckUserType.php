ba33fade752b94b9070bf56c1569485c79cee744
Afonso Fernandes <dom.afonsoduarte@gmail.com>
2024-06-17 13:27:57 +0100

    S

---
+++ app/Http/Middleware/CheckUserType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+class CheckUserType
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $user = Auth::user();
+
+        if ($user && in_array(strtoupper($user->type), ['A', 'E'])) {
+            return $next($request);
+        }
+
+        return redirect('/');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,8 @@ use App\Http\Controllers\PurchaseController;
 use App\Http\Controllers\PDFControllerView;
 use App\Http\Controllers\StatisticsController;
 use App\Http\Controllers\ParametersController;
-
+use App\Http\Middleware\CheckUserType;
+use App\Http\Middleware\CheckUserTypeAdvanced;
 
 /* ----- PUBLIC ROUTES ----- */
 
@@ -88,106 +89,128 @@ Route::middleware(['auth', 'verified'])->group(function () {
         return redirect('movies/showcase');
     })->name('dashboard');
 
-    Route::post('/statistic/chosen', [StatisticsController::class, 'redirectioning'])->name('statistics.day');
-    Route::get('statistics', [StatisticsController::class, 'index'])
-        ->name('statistics.index');
-    // ->can('viewStatistics', User::class);
-    Route::post('/export-statistics', [StatisticsController::class, 'exportToExcel'])->name('statistics.export');
-
+    
 
     //Purchases
     Route::get('purchases/my', [PurchaseController::class, 'myPurchases'])
             ->name('purchases.my');
-    Route::resource('purchases', PurchaseController::class)->except(['store']);
+    
 
-    //Tickets
-    Route::get('tickets/all', [TicketController::class, 'everyIndex'])->name('tickets.all');
-    Route::post('/tickets/verify/{screening}', [TicketController::class, 'verify'])->name('tickets.verify');
-    Route::post('/tickets/validate/{ticket}', [TicketController::class, 'validate'])->name('tickets.validate');
-
-    // MOVIES
-    Route::get('movies/create', [MovieController::class, 'create'])->name('movies.create');
-    Route::get('movies/deleted', [MovieController::class, 'indexDeleted'])->name('movies.deleted')->withTrashed();
-
-    Route::patch('movies/deleted/{movie}/save', [MovieController::class, 'save'])->name('movies.save')->withTrashed();
-
-    Route::delete('movies/{movie}/permanent-delete', [MovieController::class, 'destruction'])->name('movies.permanent-delete')->withTrashed();
-
-    Route::delete('movies/{movie}/permanent-delete-forced', [MovieController::class, 'destructionForced'])->name('movies.permanent-delete-forced')->withTrashed();
-
-    Route::delete('movies/{movie}/image', [MovieController::class, 'destroyImage'])->name('movies.image.destroy')->can('update', Movie::class);
-    Route::resource('movies', MovieController::class)->except(['show']);
-
-
-    //GENRES
-    Route::patch('genres/deleted/{genre}/save', [GenreController::class, 'save'])
-        ->name('genres.save')->withTrashed();
-    Route::delete('genres/{genre}/permanent-delete', [GenreController::class, 'destruction'])
-        ->name('genres.permanent-delete')->withTrashed();
-    Route::get('genres/deleted', [GenreController::class, 'indexDeleted'])
-        ->withTrashed()->name('genres.deleted');
-    Route::resource('genres', GenreController::class);
-
-    //Screenings
-    Route::get('screenings/management', [ScreeningController::class, 'management'])->name('screenings.management');
-    Route::resource('screenings', ScreeningController::class);//->except(['show']);
-
-
-    // CUSTOMERS
-    Route::delete('customers/{customer}/photo', [CustomerController::class, 'destroyPhoto'])
-        ->name('customers.photo.destroy')
-        ->can('update', 'customer');
-    Route::post('customers/deleted/{customer}/block', [CustomerController::class, 'invertBlockTrash'])
-        ->withTrashed()
-        ->name('customers.deleted.invert');
-    Route::post('customers/{customer}/block', [CustomerController::class, 'invertBlock'])->name('customers.invert');
-    Route::get('customers/deleted', [CustomerController::class, 'indexDeleted'])
-        ->withTrashed()
-        ->name('customers.deleted');
-    Route::patch('customers/deleted/{customer}/save', [CustomerController::class, 'save'])
-        ->name('customers.save')->withTrashed();
-    Route::delete('customers/{customer}/permanent-delete', [CustomerController::class, 'destruction'])
-        ->name('customers.permanent-delete')
-        ->withTrashed();
-
-    Route::resource('customers', CustomerController::class);
-
-    //Administratives
-    Route::delete('administratives/{administrative}/photo', [AdministrativeController::class, 'destroyPhoto'])->name('administratives.photo.destroy');//->can('update', 'administrative');
-
-    Route::patch('administratives/deleted/{user}/save', [AdministrativeController::class, 'save'])->withTrashed()->name('administratives.save');
-    Route::delete('administratives/{user}/permanent-delete', [AdministrativeController::class, 'destruction'])->withTrashed()->name('administratives.permanent-delete');
-    Route::get('administratives/deleted', [AdministrativeController::class, 'indexDeleted'])->withTrashed()->name('administratives.deleted');
-    //Admnistrative resource routes are protected by AdministrativePolicy on the controller
-    Route::resource('administratives', AdministrativeController::class);
+    
 
 
     // Confirm (store) the cart and save Screenings registration on the database:
     Route::post('cart', [CartController::class, 'confirm'])
         ->name('cart.confirm')
         ->can('confirm-cart');
-
-    //Configurations
-    Route::get('parameters', [ParametersController::class, 'index'])->name('parameters.index');
-    Route::post('/update-ticket-price', [ParametersController::class, 'updateTicketPrice'])->name('updateTicketPrice');
-    Route::post('/update-ticket-discount', [ParametersController::class, 'updateDiscount'])->name('updateDiscount');
-        
+    
+    
+    
 
 
-    //Theaters
-    Route::delete('theaters/{theater}/seat', [SeatController::class, 'destroyUpdate'])->name('seats.destroyAll');
-    Route::get('theaters/deleted', [TheaterController::class, 'deleted'])
-        ->name('theaters.deleted');
-    Route::patch('theaters/deleted/{theater}/save', [TheaterController::class, 'saveD'])
-        ->name('theaters.save')->withTrashed();
-    Route::delete('theaters/{theater}/permanent-delete', [TheaterController::class, 'destructionD'])
-        ->name('theaters.permanent-delete')->withTrashed();
-    Route::delete('theater/{theater}/image', [TheaterController::class, 'destroyImage'])
-        ->name('theaters.image.destroy')
-        ->can('update', Movie::class);
-    Route::resource('theaters', TheaterController::class);
+    Route::middleware([CheckUserTypeAdvanced::class])->group(function () {
+
+        //Purchases
+        Route::resource('purchases', PurchaseController::class)->except(['store']);
+        //Tickets
+        Route::get('tickets/all', [TicketController::class, 'everyIndex'])->name('tickets.all');
 
 
+        // MOVIES
+        Route::get('movies/create', [MovieController::class, 'create'])->name('movies.create');
+        Route::get('movies/deleted', [MovieController::class, 'indexDeleted'])->name('movies.deleted')->withTrashed();
+
+        Route::patch('movies/deleted/{movie}/save', [MovieController::class, 'save'])->name('movies.save')->withTrashed();
+
+        Route::delete('movies/{movie}/permanent-delete', [MovieController::class, 'destruction'])->name('movies.permanent-delete')->withTrashed();
+
+        Route::delete('movies/{movie}/permanent-delete-forced', [MovieController::class, 'destructionForced'])->name('movies.permanent-delete-forced')->withTrashed();
+
+        Route::delete('movies/{movie}/image', [MovieController::class, 'destroyImage'])->name('movies.image.destroy')->can('update', Movie::class);
+        Route::resource('movies', MovieController::class)->except(['show']);
+
+
+        //GENRES
+        Route::patch('genres/deleted/{genre}/save', [GenreController::class, 'save'])
+            ->name('genres.save')->withTrashed();
+        Route::delete('genres/{genre}/permanent-delete', [GenreController::class, 'destruction'])
+            ->name('genres.permanent-delete')->withTrashed();
+        Route::get('genres/deleted', [GenreController::class, 'indexDeleted'])
+            ->withTrashed()->name('genres.deleted');
+        Route::resource('genres', GenreController::class);
+
+        //Screenings
+        Route::get('screenings/management', [ScreeningController::class, 'management'])->name('screenings.management');
+        Route::resource('screenings', ScreeningController::class)->except(['index','show']);
+
+        // CUSTOMERS
+        Route::delete('customers/{customer}/photo', [CustomerController::class, 'destroyPhoto'])
+        ->name('customers.photo.destroy')
+        ->can('update', 'customer');
+        Route::post('customers/deleted/{customer}/block', [CustomerController::class, 'invertBlockTrash'])
+            ->withTrashed()
+            ->name('customers.deleted.invert');
+        Route::post('customers/{customer}/block', [CustomerController::class, 'invertBlock'])->name('customers.invert');
+        Route::get('customers/deleted', [CustomerController::class, 'indexDeleted'])
+            ->withTrashed()
+            ->name('customers.deleted');
+        Route::patch('customers/deleted/{customer}/save', [CustomerController::class, 'save'])
+            ->name('customers.save')->withTrashed();
+        Route::delete('customers/{customer}/permanent-delete', [CustomerController::class, 'destruction'])
+            ->name('customers.permanent-delete')
+            ->withTrashed();
+
+        Route::resource('customers', CustomerController::class);
+
+        //Administratives
+        Route::delete('administratives/{administrative}/photo', [AdministrativeController::class, 'destroyPhoto'])->name('administratives.photo.destroy');//->can('update', 'administrative');
+
+        Route::patch('administratives/deleted/{user}/save', [AdministrativeController::class, 'save'])->withTrashed()->name('administratives.save');
+        Route::delete('administratives/{user}/permanent-delete', [AdministrativeController::class, 'destruction'])->withTrashed()->name('administratives.permanent-delete');
+        Route::get('administratives/deleted', [AdministrativeController::class, 'indexDeleted'])->withTrashed()->name('administratives.deleted');
+        //Admnistrative resource routes are protected by AdministrativePolicy on the controller
+        Route::resource('administratives', AdministrativeController::class);
+
+
+
+        //Statistics
+        Route::post('/statistic/chosen', [StatisticsController::class, 'redirectioning'])->name('statistics.day');
+        Route::get('statistics', [StatisticsController::class, 'index'])
+            ->name('statistics.index');
+        // ->can('viewStatistics', User::class);
+        Route::post('/export-statistics', [StatisticsController::class, 'exportToExcel'])->name('statistics.export');
+
+
+        //Configurations
+        Route::get('parameters', [ParametersController::class, 'index'])->name('parameters.index');
+        Route::post('/update-ticket-price', [ParametersController::class, 'updateTicketPrice'])->name('updateTicketPrice');
+        Route::post('/update-ticket-discount', [ParametersController::class, 'updateDiscount'])->name('updateDiscount');
+
+        //Theaters
+        Route::delete('theaters/{theater}/seat', [SeatController::class, 'destroyUpdate'])->name('seats.destroyAll');
+        Route::get('theaters/deleted', [TheaterController::class, 'deleted'])
+            ->name('theaters.deleted');
+        Route::patch('theaters/deleted/{theater}/save', [TheaterController::class, 'saveD'])
+            ->name('theaters.save')->withTrashed();
+        Route::delete('theaters/{theater}/permanent-delete', [TheaterController::class, 'destructionD'])
+            ->name('theaters.permanent-delete')->withTrashed();
+        Route::delete('theater/{theater}/image', [TheaterController::class, 'destroyImage'])
+            ->name('theaters.image.destroy')
+            ->can('update', Movie::class);
+        Route::resource('theaters', TheaterController::class);
+
+
+    });
+
+    Route::middleware([CheckUserType::class])->group(function () {
+        //Tickets
+        Route::post('/tickets/verify/{screening}', [TicketController::class, 'verify'])->name('tickets.verify');
+        Route::post('/tickets/validate/{ticket}', [TicketController::class, 'validate'])->name('tickets.validate');
+        //Screenings
+        Route::resource('screenings', ScreeningController::class)->only(['index','show']);
+
+    });
+    
 
 });
 
